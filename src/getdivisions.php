5851eb2263f7cd491805a0c49ec30c607b9d7733
Michael Schär <michael.schaer@pbs.ch>
2021-08-27 11:33:39 +0200

    Create getdivisions.php
    Wir brauchen noch ein Update-Script für die Webseite, damit sie die Änderungen abholen kann. 
    
    Das hier sollte eigentlich reichen, es imitiert den Output des bisher eingesetzten Scripts auf unserem Azure.

---
+++ src/getdivisions.php
@@ -0,0 +1,44 @@
+<?php
+include './database.php';
+
+function getLocations($connection, $divisionCode) {
+  $sql_loc = "SELECT * FROM locations WHERE code = " . $divisionCode;
+  $result_loc = $connection->query($sql_loc);
+  $locations = $result_loc->fetch_all(MYSQLI_ASSOC);
+  
+  foreach($locations as &$loc){   
+    $loc["id"] = intval($loc["id"]);
+    $loc["latitude"] = floatval($loc["latitude"]);
+    $loc["longitude"] = floatval($loc["longitude"]);
+    unset($loc["pbs_id"]);
+  }
+  
+  return $locations;
+}
+
+function getDivisions($connection) {
+  $sql = "SELECT * FROM divisions";
+  $result = $connection->query($sql);
+
+  if ($result->num_rows > 0) {
+    $divisions = $result->fetch_all(MYSQLI_ASSOC);
+    
+    foreach($divisions as &$div){     
+      $div["gender"] = intval($div["gender"]);
+      $div["pta"] = boolval($div["pta"]);
+      unset($div["pbs_id"]);
+      unset($div["updated_at"]);
+      $div["locations"] = getLocations($connection, $div["code"]);
+    }
+    
+    return json_encode($divisions);
+  } else {
+    return "no results found";
+  }
+}
+
+$connection = connect($config);
+header('Content-Type: application/json; charset=UTF-8');
+echo getDivisions($connection);
+
+?>
