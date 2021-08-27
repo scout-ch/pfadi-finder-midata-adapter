6b89811932f15cce4408cb3d32456bf006017cef
Michael Schär <michael.schaer@pbs.ch>
2021-08-27 12:45:41 +0200

    Update getdivisions.php
    - DivisionCode braucht marks für den Vergleich
    - Floatval liefert 14-stellige Zahlen, wir brauchen aber 6 Stellen

--- src/getdivisions.php
+++ src/getdivisions.php
@@ -2,14 +2,14 @@
 include './database.php';
 
 function getLocations($connection, $divisionCode) {
-  $sql_loc = "SELECT * FROM locations WHERE code = " . $divisionCode;
+  $sql_loc = "SELECT * FROM locations WHERE code = '" . $divisionCode . "'";
   $result_loc = $connection->query($sql_loc);
   $locations = $result_loc->fetch_all(MYSQLI_ASSOC);
   
   foreach($locations as &$loc){   
     $loc["id"] = intval($loc["id"]);
-    $loc["latitude"] = floatval($loc["latitude"]);
-    $loc["longitude"] = floatval($loc["longitude"]);
+    $loc["latitude"] = number_format($loc["latitude"],6,".","");
+    $loc["longitude"] = number_format($loc["longitude"],6,".","");
     unset($loc["pbs_id"]);
   }
   
