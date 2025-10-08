3ea9776e0be3962c5dbe5b97e262e3dbae681153
DominikFischli <dominikfischli@bluewin.ch>
2025-10-08 12:19:35 +0200

    feat: Check if columns exist when migrating

--- src/migrate.php
+++ src/migrate.php
@@ -2,14 +2,36 @@
 
 include './database.php';
 
+
+function ensureColumnExists($connection,$table, $column, $column_definition) {
+  $query = <<<CHECK
+  SELECT COUNT(*)
+  FROM information_schema.COLUMNS
+  WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = "$table"
+    AND COLUMN_NAME = "$column";
+  CHECK;
+
+  $table_exists = boolval($connection->query($query)->fetch_row()[0]);
+  if(!$table_exists) {
+    $query = 'ALTER TABLE `' . $table . '` ADD ' . $column . " " . $column_definition . ";";
+    $connection->query($query);
+  }
+}
+
 function addUpdatedAt($connection) {
-  $connection->query("ALTER TABLE `divisions` ADD updated_at DATETIME NULL;");
+  ensureColumnExists($connection, "divisions", "updated_at", "DATETIME NULL");
 }
 
 function addPbsId($connection) {
-  $connection->query("ALTER TABLE `divisions` ADD pbs_id INT(11) UNSIGNED;");
-  $connection->query("ALTER TABLE `locations` ADD pbs_id INT(11) UNSIGNED;");
+  ensureColumnExists($connection, "divisions", "pbs_id", "INT(11) UNSIGNED");
+  ensureColumnExists($connection, "locations", "pbs_id", "INT(11) UNSIGNED");
+}
+
+function addDescription($connection) {
+  ensureColumnExists($connection, "divisions", "description", "VARCHAR(255) NULL");
 }
 
 addUpdatedAt(connect($config));
 addPbsId(connect($config));
+addDescription(connect($config));
