2a638b4939da887e81106fffadc14270d3b09543
DominikFischli <dominikfischli@bluewin.ch>
2025-12-03 19:11:47 +0100

    fix: correctly validate password and username

--- src/auth.php
+++ src/auth.php
@@ -5,9 +5,17 @@
  */
 
 
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
+if (!isset($_SERVER['PHP_AUTH_USER']) or !isset($_SERVER['PHP_AUTH_PW'])) {
   header('WWW-Authenticate: Basic realm="pfadi-finder-midata-adapter"');
   header('HTTP/1.0 401 Unauthorized');
-  echo 'Authorization required';
+  print("Authentication required.");
   exit;
+}
+
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+
+if ($user !== $config['AUTH_USER'] || $pass !== $config['AUTH_PASSWORD']) {
+  header('HTTP/1.0 401 Unauthorized');
+  die("Not authorized");
 }
